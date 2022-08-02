52eae444a51eec19ed3decb74513255e7cd1b328
Luke Renoe <lukerenoe@gmail.com>
2022-08-02 16:20:49 -0600

    page structure, install dependencies

---
+++ my-app/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import './components.css'
+function Nav() {
+  return (
+    <div className="main-container">
+     
+<a href='/home'>Home</a>
+<a href='/contact'>Contact</a>
+<a href='/fundraising'>Fundraising</a>
+<a href='/mission'>Mission</a>
+
+    </div>
+  );
+}
+
+export default Nav;
