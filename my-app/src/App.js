8594f04736ea385f223fea4f64e16d4343d6298b
Luke Renoe <lukerenoe@gmail.com>
2022-08-02 16:30:02 -0600

    css framework

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Mission from './components/Mission';
+import News from './components/News';
 import Fundraising from './components/Fundraising';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
@@ -20,6 +22,12 @@ path='/fundraising' element={<Fundraising/>}
 />
 <Route
 path='/contact' element={<Contact/>}
+/>
+<Route
+path='/mission' element={<Mission/>}
+/>
+<Route
+path='/news' element={<News/>}
 />
       </Routes>
       </BrowserRouter>
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -9,15 +9,15 @@ function Home() {
         <article>
             <a href="https://www.lrenoe.com">
             <section  id="Portfolio" class="card1" >
-                <h2 class="cardheader">Art</h2> 
+                <h2 class="cardheader">Mission</h2> 
             </section></a>
             <a href="https://almost-faim-ous.herokuapp.com/chat">
             <section class="card2">
-                <h3 class="cardheader">Almost fAIM-ous</h3>
+                <h3 class="cardheader">News</h3>
             </section></a>
             <a href="https://www.lrenoe.com/selected-works">
             <section class="card3">
-                <h4 class="cardheader">More Art</h4>
+                <h4 class="cardheader">Fundraising</h4>
             </section></a>
         </article>
     
