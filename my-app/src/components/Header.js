524c9fafd2807cd85027b350520d7ab54eeb6348
Luke Renoe <lukerenoe@gmail.com>
2022-08-10 16:56:07 -0600

    underground styling test

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -5,7 +5,17 @@ function Header() {
   return (
     <div className="header">
      <h1 className="h1">Grundeinkommen's Graswurzel</h1>
+     
+     <div className='vision'>Vision: A society guaranteeing every member a periodic payment, enough to enable life in dignity without the necessity to provide anything in return.
+<br/>
+A transformed society of free individuals who have options, express their creative capacities, pursue meaningful action, spend more time with their families, and give back to society. 
+<br/>
+A fairer and more equitable world as a result of others emulating the German model of UBI after it has proven successful.
+<br/>
+Through new methods of political canvassing and by promoting a new narrative around UBI, we will show citizens and leaders alike that this is the detour we must take to prepare ourselves for the future and create a more equitable world.
+    </div>
      <Nav></Nav>
+
     </div>
   );
 }
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -6,21 +6,7 @@ function Home() {
 
 
 <div> 
-        {/* <article>
-            <a href="https://www.lrenoe.com">
-            <section  id="Portfolio" class="card1" >
-                <h2 class="cardheader">Mission</h2> 
-            </section></a>
-            <a href="https://almost-faim-ous.herokuapp.com/chat">
-            <section class="card2">
-                <h3 class="cardheader">News</h3>
-            </section></a>
-            <a href="https://www.lrenoe.com/selected-works">
-            <section class="card3">
-                <h4 class="cardheader">Fundraising</h4>
-            </section></a>
-        </article> */}
-    
+
 </div>
 
     </div>
