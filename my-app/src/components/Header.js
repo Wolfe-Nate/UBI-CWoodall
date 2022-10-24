c858d12490261e0271786f980cf85d5f90244957
Luke Renoe <lukerenoe@gmail.com>
2022-10-24 17:45:12 -0600

    merge commit

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import Nav from './Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 // import Image from 'react-bootstrap/Button';
 // import Footer from './Footer';
 // import Apple from '../Icons'
+
 function Header() {
   return (
     <div>
+     
       <Nav></Nav>
-    
+       
+
+
 <div className='container'> 
   <div className='what-is-ubi'>
   <h2>WHAT IS UNIVERSAL BASIC INCOME? </h2>
-
+  {/* <button><Link to="/German"> German </Link></button> */}
+  <a href="/German">Translate</a>
 <h4>A society that guarantees every member a periodic payment, enough to enable life in dignity without the necessity to provide anything in return. 
 </h4>
 <div className='what-is-ubi' >
@@ -126,18 +137,18 @@ Add your email to our supporters' list to receive updates from our campaign, suc
 <div className='about-us'>
 <div>
 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqwX4NnEoLQzUoRBuuXY-51GmaYemDy6miXdjTPaNxAg&s' width={300} alt="person"/>
-<div className='bios-name'>Connor Woodall</div>
-<div className='bio'>Connor is an activist and organizer who has found his passion in this work and the people in these spaces. He has worked in non-profit and international organizations and has spent time in local, county, and state governments. With a Master's in Political Science, he has become a huge proponent of system thinking and innovative ways to design our world to move Humanity to a more hopeful future.</div>
+<div className='bios-name'>Tom Peterson</div>
+<div className='bio'>I am the Founder and Lead of this campaign. I am 21 years old and recently moved to Berlin to work toward promoting a UBI. My political experience is primarily as an activist here in Germany, where I have been an organizer for climate strikes in Bad Oldesloe, where I grew up. I have acquired vital experiences and resources from building a Deep Canvassing campaign at Zukunftswahl/Justice is Global Europe and my internship with Justice is Global US. In JIG US, I was a team lead on our US-China Narratives Deep Canvassing project. My goal is to see a world less tied to money and more tied to individual, community, and world progress.  </div>
+</div>
+<div>
+<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqwX4NnEoLQzUoRBuuXY-51GmaYemDy6miXdjTPaNxAg&s' width={300} alt="person"/>
+<div className='bios-name'>Daphne Nounesi</div>
+<div className='bio'>I am 27 and based in Greece and Canada. I have always been concerned with socio-political affairs and matters of economic justice and I have a Master’s degree in Political Science from the Geneva Graduate Institute. I have experience as a deep canvasser with JIG/People’s Action and have worked with several grassroots and international organizations in grants and project management, administration, diplomacy, and research. As the campaign’s Project Manager, I worked to develop organizational strategy and oversaw task management.</div>
 </div>
 <div>
 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqwX4NnEoLQzUoRBuuXY-51GmaYemDy6miXdjTPaNxAg&s' width={300} alt="person"/>
 <div className='bios-name'>Connor Woodall</div>
-<div className='bio'>Connor is an activist and organizer who has worked in non-profit and international organizations. He has spent time in local, county, and state governments. With a Master's in Political Science, he has become a huge proponent of system thinking and innovative ways to design our world to move Humanity to a more hopeful future.</div>
-</div>
-<div>
-<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqwX4NnEoLQzUoRBuuXY-51GmaYemDy6miXdjTPaNxAg&s' width={300} alt="person"/>
-<div className='bios-name'>Connor Woodall</div>
-<div className='bio'>Connor is an activist and organizer who has found his passion in this work and the people in these spaces. He has worked in non-profit and international organizations and has spent time in local, county, and state governments. With a Master's in Political Science, he has become a huge proponent of system thinking and innovative ways to design our world to move Humanity to a more hopeful future.</div>
+<div className='bio'>I am 28 and from Rhode Island, USA. My role on this team is the Deep Canvassing Coordinator as well as the Head of Narrative Development. I have a Master’s in Political Science from the University of Colorado Denver. My experiences range from local and state government as well as national and international non-profit progressive organizing. I bring to this team my knowledge of the nuances of human systems, my experience with Deep Canvassing, and my ability to influence others to think differently.</div>
 </div>
 </div>
 
--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import German from './German';
+// import German from './German';
+import {Link} from "react-router-dom";
 // import './components.css'
 function Nav() {
   return (
@@ -8,7 +9,6 @@ function Nav() {
 {/* <a className="nav" href='/home'>Home</a> */}
 <a className="nav" href='#mission'>Mission</a>
 <a className="nav" href='#support'>Support Us </a>
-{/* <button><a className="nav" href={German.js}>German</a></button> */}
 {/* <a className="nav" href='/contact'>Contact </a> */}
 
     </div>
